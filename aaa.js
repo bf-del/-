0b523c7be1496da389292f752f584b895defecd3
libai <12345>
2019-12-19 17:20:54 +0800

    test

---
+++ aaa.js
@@ -0,0 +1,18 @@
+
+var yu = 1;
+   function fn() {
+       console.log('函数被调用了');
+   }
+   fn();
+   for(var i =0;i<5;i++){
+       console.log(i);
+   }
+   if(true){
+       console.log('123');
+       
+   }
+   const a = require ('./bbb.js')
+   console.log(a.add(2,2));
+   console.log('12344');
+   
+   
